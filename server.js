c57f513273fb050c3263a31e4f85b373c5b6c5e2
Ian <ian_dowdy@outlook.com>
2018-12-18 20:58:50 -0600

    changessss

--- server.js
+++ server.js
@@ -16,8 +16,8 @@ if (process.env.NODE_ENV === 'production') {
     app.use(express.static('client/build'));
 
     const path = require('path');
-    app.get('/index', (req, res) => {
-        res.sendFile(path.resolve(__dirname, './client/build/index.html'));
+    app.get('/', (req, res) => {
+        res.sendFile(path.join(__dirname, './client/build/index.html'));
     })
 }
 
